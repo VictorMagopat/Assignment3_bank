e2c2364d5906cfb99d32d93a659abf67c249ebcb
VictorMagopat <magopat.victor@gmail.com>
2023-11-11 07:42:27 -0800

    Updated bank functionality
    Added new methods: openAccountChecquing, openAccountSaving, searchAccountByNo, searchAccountByName. Renamed the test accounts to generic names Acc_01, ..,Acc_06.

--- bank.py
+++ bank.py
@@ -7,26 +7,32 @@ import account
 print("Testing the bank implementation!")
     
 # predefined Saving accounts
-Saving_TomWest = account.SavingAccount()
-Saving_TomWest.setAccountHolderName("Tom West")
-Saving_JohnNorth = account.SavingAccount()
-Saving_JohnNorth.setAccountHolderName("John North")
-Saving_MikeSouth = account.SavingAccount()
-Saving_MikeSouth.setAccountHolderName("Mike South")
+Acc_01 = account.SavingAccount()
+Acc_01.setAccountHolderName("Tom West")
+Acc_01.setAccountNumber(1)
+Acc_02 = account.SavingAccount()
+Acc_02.setAccountHolderName("John North")
+Acc_02.setAccountNumber(2)
+Acc_03 = account.SavingAccount()
+Acc_03.setAccountHolderName("Mike South")
+Acc_03.setAccountNumber(3)
 
 # predefined Chequing accounts
-Chequing_SarahWest = account.ChequingAccount()
-Chequing_SarahWest.setAccountHolderName("Sarah West")
-Chequing_AnneNorth = account.ChequingAccount()
-Chequing_AnneNorth.setAccountHolderName("Anne North")
-Chequing_JaneSouth = account.ChequingAccount()
-Chequing_JaneSouth.setAccountHolderName("Jane South")
+Acc_04 = account.ChequingAccount()
+Acc_04.setAccountHolderName("Sarah West")
+Acc_04.setAccountNumber(4)
+Acc_05 = account.ChequingAccount()
+Acc_05.setAccountHolderName("Anne North")
+Acc_05.setAccountNumber(5)
+Acc_06 = account.ChequingAccount()
+Acc_06.setAccountHolderName("Jane South")
+Acc_06.setAccountNumber(6)
 
-ExistingAccounts = [Saving_TomWest, Saving_JohnNorth, Saving_MikeSouth, Chequing_SarahWest, Chequing_AnneNorth, Chequing_JaneSouth]
+ExistingAccounts = [Acc_01, Acc_02, Acc_03, Acc_04, Acc_05, Acc_06]
 
 
 class Bank:
-    def __init__(self, existingAccounts = [], lastAccountNumber = 0, bankName = "No Name") -> None:
+    def __init__(self, existingAccounts = [], lastAccountNumber = 0, bankName = "Famous Bank") -> None:
         
         # this keeps track of all the accounts
         self.lastAccountNumber: int  
@@ -39,9 +45,9 @@ class Bank:
         # Define a constructor that populates the account list with hardcoded of three 
         # ChequingAccount instances and three SavingsAccount instances. 
         listLenght = len(existingAccounts)
-        self.lastAccountNumber = 1 + listLenght 
+        self.lastAccountNumber = listLenght 
         index = 0
-        while index < listLenght:      
+        while index < listLenght:
             self.listExistingAccounts.append(existingAccounts[index])
             index += 1
         pass
@@ -52,13 +58,57 @@ class Bank:
     def getLastAccountNumber(self):
         return self.lastAccountNumber    
     
-    def openAccount():
+    # instantiates a saving account and appends it to the list
+    def openAccountSaving(self, holderName: str):
+        openNewAcc = account.SavingAccount()
+        openNewAcc.setAccountHolderName(holderName)
+        
+        self.lastAccountNumber += 1
+        openNewAcc.setAccountNumber(self.lastAccountNumber)
+        
+        self.listExistingAccounts.append(openNewAcc)        
         pass
 
-    def searchAccount():
+    # instantiates a checking account and appends it to the list
+    def openAccountChequing(self, holderName: str):
+        openNewAcc = account.ChequingAccount()
+        openNewAcc.setAccountHolderName(holderName)
+        
+        self.lastAccountNumber += 1
+        openNewAcc.setAccountNumber(self.lastAccountNumber)
+
+        self.listExistingAccounts.append(openNewAcc)
         pass
+    
+    # search database by account number
+    def searchAccountByNo(self, accNo):
+        # accFound is 1 if successful, 0 if there is no account with this number
+        accFound = 0
+        accIndex = 0
+        while accIndex < self.lastAccountNumber:
+            currAcc = self.listExistingAccounts[accIndex].getAccountNumber()
+            if accNo == currAcc:
+                accFound = 1
+                break
+            else:
+                accFound = 0
+            accIndex += 1
+        return accFound        
+
+    # search database by account
+    def searchAccountByName(self, accName):        
+        # accFound is 1 if successful, 0 if there is no account with this name
+        accFound = 0
+        accIndex = 0
+        while accIndex < self.lastAccountNumber:
+            currName = self.listExistingAccounts[accIndex].getAcountHolderName()
+            if accName == currName:
+                accFound = 1
+            accIndex += 1
+        return accFound
 
 
+# this code is testing the functionality of the bank module
 if __name__ == "__main__":
     # print the list of the test accounts
     listLenght = len(ExistingAccounts) 
@@ -80,11 +130,42 @@ if __name__ == "__main__":
     print("Bank name is: ", nameOfBank )
 
     myIndex = 0
-    while myIndex < (noOfAccounts - 1):
+    while myIndex < myBank.lastAccountNumber:
         account_name = myBank.listExistingAccounts[myIndex].getAcountHolderName()
-        print("Account name", account_name)
+        account_no = myBank.listExistingAccounts[myIndex].getAccountNumber()
+        print("Account name: ", account_name, "Account number: ", str(account_no))
         myIndex += 1
     print("Finished printing the bank list.")
 
+    searchAccNo = 1
+    print("Search by Account Number")
+    foundAcc = myBank.searchAccountByNo(searchAccNo)
+    if foundAcc == 1:
+        print("Found account number: ", str(searchAccNo))
+    else:
+        print("The account number", str(searchAccNo), "does't exist")
 
+    searchAccName = "Anne North"
+    print("Search by Account Name")
+    foundAcc = myBank.searchAccountByName(searchAccName)
+    if foundAcc == 1:
+        print("Found account: ", searchAccName)
+    else:
+        print("This account name", searchAccName, " does't exist")
+
+    newAccHolderName = "Nora East"
+    print("Create new Chequing Account for: ", newAccHolderName)
+    myBank.openAccountChequing(newAccHolderName)
+
+    newAccHolderName = "Rick East"
+    print("Create new Saving Account for: ", newAccHolderName)
+    myBank.openAccountSaving(newAccHolderName)
+
+    myIndex = 0
+    while myIndex < myBank.lastAccountNumber:
+        account_name = myBank.listExistingAccounts[myIndex].getAcountHolderName()
+        account_no = myBank.listExistingAccounts[myIndex].getAccountNumber()
+        print("Account name: ", account_name, "Account number: ", str(account_no))
+        myIndex += 1
+    print("Finished printing the updated bank list.")
 
