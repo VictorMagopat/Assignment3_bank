c3a035ab249bcd93e06405989d296c427cf058a7
VictorMagopat <magopat.victor@gmail.com>
2023-11-12 04:45:09 -0800

    Implemented deposit and withdraw
    Implemented deposit and withdraw. The main menu was updated: 1. Select Chequing Account; 2. Select Saving Account. After this selection the user has the option to search by Account Number or Account Name using the AccountSearchMenu.

--- teller.py
+++ teller.py
@@ -4,7 +4,7 @@
 # Implements all the interaction between the user and the Bank. 
 # No other class is allowed to interact (input or print) with the end user. 
 
-
+import account
 import bank
 
 # this is the welcome string
@@ -15,20 +15,26 @@ GoodbyeMessage = "Have a nice day.\nGoodbye!"
 
 # this is the main menu 
 OptionsMainMenu = """Bank main menu options:
-            <1> Select Account by <Number>
-            <2> Select Account by <Name>
+            <1> Select Chequing Account
+            <2> Select Saving Account
             <3> Open Account
             <4> Exit"""
 
+# this is the main menu 
+AccountSearchMenu = """Search options:
+            <1> Search Account by <Number>
+            <2> Search Account by <Name>
+            <3> Return to main menu"""
+
 # this is the account menu 
-OptionsAccountMenu = """Account menu:
+AccountOperationsMenu = """Account menu:
             <1> Check Balance
             <2> Deposit
             <3> Withdraw
             <4> Return to main menu"""
 
 
-OptionAccountType = """What account type would like to open:
+AccountTypeMenu = """What account type would like to open:
             <1> Saving Account  
             <2> Chequing Account\n"""
 
@@ -59,28 +65,57 @@ class Application:
     # 3. Exit: allows the user to exit the application
     def showMainMenu(self):
         tellerOn = True
+        accType = 0
         while tellerOn == True:
             print(OptionsMainMenu)
             menu_start = input("What is your selection: ")
-            if menu_start == "1":
-                print("Select account by <Number>: ")
-                accInput = input("Please enter the number of the account: \n")            
-                accNo = int(accInput)
-                self.serveAccIndex = self.famousBank.searchAccountByNo(accNo)
-                if self.serveAccIndex < 0:
-                    print("This account doesnt exist")
-                else:
-                    self.showAccountMenu()
 
-            if menu_start == "2":
-                print("Select account by <Name> ")
-                accName = input("Please enter the name of the account: \n")
-                self.serveAccIndex = self.famousBank.searchAccountByName(accName)
-                if self.serveAccIndex < 0:
-                    print("This account doesnt exist")
+            # search account
+            if (menu_start == "1") or (menu_start == "2"):
+                if(menu_start == "1"):
+                    print("You are looking for a Chequing Account: ")
+                    accType = account.ACC_TYPE_CHEQUING
                 else:
-                    self.showAccountMenu()
- 
+                    print("You are looking for a Saving Account: ")
+                    accType = account.ACC_TYPE_SAVING 
+                searchOn = True
+                while searchOn == True:
+                    print(AccountSearchMenu)
+                    menu_start = input("What is your selection: ")
+
+                    # search account by number
+                    if menu_start == "1":
+                        print("Select account by <Number>: ")
+                        accInput = input("Please enter the number of the account: \n")            
+                        accNo = int(accInput)
+                        print("Searching for Chequing account number: ", str(accNo))
+                        self.serveAccIndex = self.famousBank.searchAccountByNo(accNo, accType)
+                        if self.serveAccIndex < 0:
+                            print("This account doesnt exist")
+                        else:
+                            self.showAccountMenu()
+                    
+                    # search account by name
+                    elif menu_start == "2":
+                        print("Select account by <Name> ")
+                        accName = input("Please enter the name of the account: \n")
+                        self.serveAccIndex = self.famousBank.searchAccountByName(accName, accType)
+                        print("Searching for Chequing account name: ", accName)
+                        if self.serveAccIndex < 0:
+                            print("This account doesnt exist")
+                        else:
+                            self.showAccountMenu()
+
+                    # return to main menu
+                    elif menu_start == "3":
+                        print("You selected return to main menu")
+                        searchOn = False
+                    elif menu_start == "q":
+                        print(GoodbyeMessage)
+                        exit()
+                    else:
+                        print("Please enter: <1>, <2> or <3>") 
+
             elif menu_start == "3":
                 print("Opening a new account.")
                 self.showOpenAccountMenu()
@@ -107,28 +142,65 @@ class Application:
     def showAccountMenu(self):
         servingAccount = True
         while servingAccount == True:
-            print(OptionsAccountMenu)
+            print(AccountOperationsMenu)
             menu_start = input("What is your selection: ")
+
+            # print the current balance
             if menu_start == "1":                
                 balance = self.famousBank.databaseAcc[self.serveAccIndex].getCurrentBalance()
                 print("Your account has a balance of: ", str(balance))
 
+            # process the deposit operation
             elif menu_start == "2":
-                deposit = 100
+                deposit = 0
+                while True:
+                    print("What amount would you like to deposit?")
+                    inputSum = input()
+                    try:
+                        deposit = float(inputSum)
+                        if (deposit < 0):
+                            print("This is invalid. Please enter a valid number.")
+                        else:
+                            break
+                    except ValueError:
+                        print("This is invalid. Please enter a valid number.")
+
+                deposit = round(deposit, 2)
                 self.famousBank.databaseAcc[self.serveAccIndex].deposit(deposit)
                 print("You deposited: ", str(deposit))
                 balance = self.famousBank.databaseAcc[self.serveAccIndex].getCurrentBalance()
                 print("Your account has a new balance of: ", str(balance))
 
+            # process the withdraw operation
             elif menu_start == "3":
-                withdraw = 50
-                print("You withdraw: ", str(withdraw))
-                self.famousBank.databaseAcc[self.serveAccIndex].withdraw(withdraw)
-                balance = self.famousBank.databaseAcc[self.serveAccIndex].getCurrentBalance()
-                print("Your account has a new balance of: ", str(balance))
+                withdraw = 0
+                while True:
+                    print("What amount would you like to withdraw?")
+                    inputSum = input()
+                    try:
+                        withdraw = float(inputSum)
+                        if (withdraw < 0):
+                            print("This is invalid. Please enter a valid number.")
+                        else:
+                            break
+                    except ValueError:
+                        print("This is invalid. Please enter a valid number.")
 
+                withdraw = round(withdraw, 2)
+                print("The sum ", str(withdraw), "will be withdrawn from account ", str(self.serveAccIndex))
+                transaction = False
+                print("You withdraw: ", str(withdraw))
+
+                transaction = self.famousBank.databaseAcc[self.serveAccIndex].withdraw(withdraw)
+                if (transaction == account.TRANSACTION_FAILED):
+                    print("This transaction failed.")
+                else:
+                    balance = self.famousBank.databaseAcc[self.serveAccIndex].getCurrentBalance()
+                    print("Your account has a new balance of: ", str(balance))
+
+            # return to main menu
             elif menu_start == "4":
-                print("You selected return to main menu")
+                print("You selected to return to main menu")
                 servingAccount = False
                 
             elif menu_start == "q":
@@ -143,7 +215,7 @@ class Application:
         accName = input("Name: ")
         accType = 0
         while True:
-            accType = input(OptionAccountType)
+            accType = input(AccountTypeMenu)
             if accType == "1":
                 self.famousBank.openAccountSaving(accName)
                 print("Congratulation", accName, "! Your Savingng Account is opened!")
